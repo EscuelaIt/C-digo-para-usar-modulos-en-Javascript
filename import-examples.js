7dbe1f6a2f452f71ad11bf8e7d6321568d20bbeb
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2022-11-11 21:07:28 +0100

    ejemplos de imports y exports

---
+++ import-examples.js
@@ -0,0 +1,9 @@
+import * as moduleAll from './module.js';
+import modulexdfsdf from './module-default.js';
+
+
+import { o, k  } from './module.js';
+
+console.log(moduleAll);
+console.log(modulexdfsdf);
+console.log(bob);
---
+++ importar-de-sin-default-al-final.js
@@ -0,0 +1,3 @@
+import * as m from './module-sin-default-al-final.js';
+
+console.log(m.o);
